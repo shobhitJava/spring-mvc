5c564201e4dd6e9c28726c6b378a6577ddd844c4
prateek <prateek.ashtikar512@gmail.com>
2016-02-07 19:39:41 +0530

    Spring Rest + Swagger

---
+++ SpringRestExample/README.md
@@ -0,0 +1,23 @@
+URL's:
+******
+- http://localhost:8080/SpringRestExample/rest/emp/dummy
+
+- http://localhost:8080/SpringRestExample/rest/emp/create
+{"id":"1", "name":"Pankaj"}
+
+- http://localhost:8080/SpringRestExample/rest/emp/1
+
+- http://localhost:8080/SpringRestExample/rest/emp/delete/999
+
+
+Swagger URL:
+************
+http://localhost:8080/SpringRestExample/docs/
+
+
+References:
+**********
+- http://www.journaldev.com/2552/spring-restful-web-service-example-with-json-jackson-and-client-program
+- 
+
+
---
+++ SpringRestExample/src/main/java/com/journaldev/spring/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.journaldev.spring.config;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	@Autowired
+	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+		this.springSwaggerConfig = springSwaggerConfig;
+	}
+
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(".*");
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Swagger API", // title
+				"API for Swagger", // description
+				"terms.html", // termsOfServiceUrl
+				"support@support.com", // contact
+				"Commercial Proprietary", // license
+				"http://www.support.com"); // licenseUrl
+	}
+}
